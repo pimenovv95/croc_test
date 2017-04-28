3f8a0b5093537201b485d9d3d852abca36ebb17d
Michail <my@email.net>
2017-04-28 09:28:48 +0300

    обновил роутер до 4 версии

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,7 @@ import {ConnectedHelloWorlds} from './containers/HelloWorlds/HelloWorlds';
 import {ConnectedWikiSearch} from './containers/CustomWikiSearch/CustomWikiSearch';
 
 export const indexRoute = function() {
-    return <Route key="index" path="/" component={FirstPage} onEnter={function(nextState, replace) {
-
-    }}/>
-};
-
-export const defaultRoute = function() {
-    return <Route key="default" path="/index" onEnter={function(nextState, replace) {
-        replace('/');
-    }}/>
+    return <Route key="index" path="/" component={FirstPage}/>
 };
 
 export const helloWorldsRoute = function() {
